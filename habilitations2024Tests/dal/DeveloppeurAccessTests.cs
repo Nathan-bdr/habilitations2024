d282b94439ef54a36bb37a592c10af887f74a4c1
Nathan Boudier <nathan.boudier@gmail.com>
2025-05-21 16:15:02 +0200

    Mission 1.2 : Tests unitaires sur la méthode GetLesDeveloppeurs

---
+++ habilitations2024Tests/dal/DeveloppeurAccessTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using habilitations2024.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using habilitations2024.model;
+
+namespace habilitations2024.dal.Tests
+{
+    [TestClass()]
+    public class DeveloppeurAccessTests : DeveloppeurAccess
+    {
+        [TestMethod()]
+        public List<Developpeur> GetLesDeveloppeursTest(int? idProfil)
+        {
+            return GetLesDeveloppeurs(idProfil);
+        }
+
+        [TestMethod]
+        public void GetLesDeveloppeurs_AvecProfil()
+        {
+            var developerAccessTests = new DeveloppeurAccessTests();
+            int nbDeveloppeursAttendu = 6; 
+            int idProfil = 1; 
+
+            var result = developerAccessTests.GetLesDeveloppeursTest(idProfil);
+            int nbDeveloppeurs = result.Count;
+
+            Assert.AreEqual(nbDeveloppeursAttendu, nbDeveloppeurs);
+        }
+
+        [TestMethod]
+        public void GetLesDeveloppeurs_SansProfil()
+        {
+            var developerAccessTests = new DeveloppeurAccessTests();
+            int nbDeveloppeursAttendu = 20;
+
+            var result = developerAccessTests.GetLesDeveloppeursTest(null);
+            int nbDeveloppeurs = result.Count;
+
+            Assert.AreEqual(nbDeveloppeursAttendu, nbDeveloppeurs);
+        }
+    }
+}
